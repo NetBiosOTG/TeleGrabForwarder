8ec87e060b7d124c1147763762819811d55a5324
NetBiosOTG <38567925+NetBiosOTG@users.noreply.github.com>
2025-06-10 01:10:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -181,6 +181,8 @@ SOURCE_TO_TOPIC_MAPPING = {
         -1002756711574: 277,     # Bonus Codes (Topic ID)
         -1002027750840: 501,    # TipMeTips (Bonus Codes)
         -1002210132078: None,   # Hotmamajess's Playground (No Topic)
+    }
+    
 }
 
 # Legacy configuration (kept for backwards compatibility, but not used in new version)
